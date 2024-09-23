386ff9c65e855b21cd3cb5caa814a1ccc2e1f6f3
Juan Hedderich <hedderich.dev@gmail.com>
2024-09-22 20:08:44 -0400

    feat(ui): :sparkles: add create orden de servicio page

---
+++ app/(dashboard)/ordenes-de-servicio/crear/page.tsx
@@ -0,0 +1,13 @@
+import Section from '@/components/ui/Section';
+
+export default function CreateOrdenesDeServicioPage() {
+  return (
+    <Section
+      title="Ordenes de Servicio"
+      subtitle="Crear una orden de servicio."
+      description="Ingresa los datos de la orden de servicio que deseas crear."
+    >
+      <div>Crear orden de servicio</div>
+    </Section>
+  );
+}
